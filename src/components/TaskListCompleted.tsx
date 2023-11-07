d1ebe70162b6ef1d4dbcc54d08f895d4f760840f
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2023-11-07 20:24:11 +0200

    refactor taskCompleted component

--- src/components/TaskListCompleted.tsx
+++ src/components/TaskListCompleted.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TaskListCompletedProps } from '../model/TaskListCompletedProps.types'
-import { Col, Container, ListGroup, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import './TaskListCompleted.scss'
 import { CommonHeadline } from '../common/CommonHeadline'
 import { TaskProps } from '../model/TaskProps.types'
@@ -16,27 +16,23 @@ export const TaskCompletedList = ({ tasks }: TaskListCompletedProps) => {
                                 title="Completed Tasks"
                                 heading="h2"
                             />
-                            <div className="task-list-wrap">
-                                <ListGroup>
-                                    {tasks.map(
-                                        (
-                                            task: TaskProps | any,
-                                            index: number,
-                                        ) =>
-                                            task.completed && (
-                                                <div key={index}>
-                                                    <ListGroup.Item
-                                                        key={task.id}
-                                                    >
-                                                        <del>
-                                                            {task.description}
-                                                        </del>
-                                                    </ListGroup.Item>
-                                                    <hr />
+                            <div className="task-complete-wrap">
+                                {tasks.map(
+                                    (task: TaskProps | any, index: number) =>
+                                        task.completed && (
+                                            <div key={index}>
+                                                <div
+                                                    className="completed-items "
+                                                    key={task.id}
+                                                >
+                                                    <del>
+                                                        {task.description}
+                                                    </del>
                                                 </div>
-                                            ),
-                                    )}
-                                </ListGroup>
+                                                <hr />
+                                            </div>
+                                        ),
+                                )}
                             </div>
                         </>
                     ) : null}
