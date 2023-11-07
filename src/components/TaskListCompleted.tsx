d98d56f694abe83553911e2dedfa9ea5c63f0277
VentsislavKostadinov <ventsislav.kostadinov@angloamerican.com>
2023-11-07 20:17:29 +0200

    add Row and Col

--- src/components/TaskListCompleted.tsx
+++ src/components/TaskListCompleted.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TaskListCompletedProps } from '../model/TaskListCompletedProps.types'
-import { Container, ListGroup } from 'react-bootstrap'
+import { Col, Container, ListGroup, Row } from 'react-bootstrap'
 import './TaskListCompleted.scss'
 import { CommonHeadline } from '../common/CommonHeadline'
 import { TaskProps } from '../model/TaskProps.types'
@@ -8,26 +8,40 @@ import { TaskProps } from '../model/TaskProps.types'
 export const TaskCompletedList = ({ tasks }: TaskListCompletedProps) => {
     return (
         <Container>
-            {tasks.length !== 0 ? (
-                <>
-                    <CommonHeadline title="Completed Tasks" heading="h2" />
-                    <div className="task-list-wrap">
-                        <ListGroup>
-                            {tasks.map(
-                                (task: TaskProps | any, index: number) =>
-                                    task.completed && (
-                                        <div key={index}>
-                                            <ListGroup.Item key={task.id}>
-                                                <del>{task.description}</del>
-                                            </ListGroup.Item>
-                                            <hr />
-                                        </div>
-                                    ),
-                            )}
-                        </ListGroup>
-                    </div>
-                </>
-            ) : null}
+            <Row>
+                <Col>
+                    {tasks.length !== 0 ? (
+                        <>
+                            <CommonHeadline
+                                title="Completed Tasks"
+                                heading="h2"
+                            />
+                            <div className="task-list-wrap">
+                                <ListGroup>
+                                    {tasks.map(
+                                        (
+                                            task: TaskProps | any,
+                                            index: number,
+                                        ) =>
+                                            task.completed && (
+                                                <div key={index}>
+                                                    <ListGroup.Item
+                                                        key={task.id}
+                                                    >
+                                                        <del>
+                                                            {task.description}
+                                                        </del>
+                                                    </ListGroup.Item>
+                                                    <hr />
+                                                </div>
+                                            ),
+                                    )}
+                                </ListGroup>
+                            </div>
+                        </>
+                    ) : null}
+                </Col>
+            </Row>
         </Container>
     )
 }
