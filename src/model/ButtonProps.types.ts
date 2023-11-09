023bfb67ac71a85ebbb4a7e00726c82a7260488e
VentsislavKostadinov <ventsislav_kostadinov@abv.bg>
2023-11-09 11:38:27 +0200

    remove optional properties form common button

--- src/model/ButtonProps.types.ts
+++ src/model/ButtonProps.types.ts
@@ -1,8 +1,8 @@
 import { BaseProps } from './BaseProps.types'
 
 export interface ButtonProps extends BaseProps {
-    text?: string
-    variant?: string
-    type?: 'button' | 'submit'
+    text: string
+    variant: string
+    type: 'button' | 'submit'
     handleClick?: () => void
 }
